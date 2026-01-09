d84ec22ba1be4a1208fa1fa784725a82c1ef27b3
huzaifakhan42732-creator <huzaifakhan42732@gmail.com>
2026-01-09 18:09:17 +0500

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.jsx";
 import Login from "./Login";
 import Register from "./Register.jsx";
@@ -7,9 +7,16 @@ import Register from "./Register.jsx";
 const AppRoutes = () => {
   return (
     <Routes>
+      {/* Redirect root "/" to /login */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      {/* Your main routes */}
       <Route path="/app" element={<App />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      {/* Catch-all for unknown routes */}
+      <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
 };
